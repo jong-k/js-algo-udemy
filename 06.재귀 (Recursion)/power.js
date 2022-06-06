72cc0d80cd5a1d940be78f081fd3ab3b1e00964c
longlive-jonghan <kjhan710@naver.com>
2022-06-07 00:31:12 +0900

    테스트케이스 추가

--- 06.재귀 (Recursion)/power.js
+++ 06.재귀 (Recursion)/power.js
@@ -11,7 +11,7 @@ function power(a, b) {
   return a * power(a, b);
 }
 
-console.log(power(2, 0));
-console.log(power(2, 1));
-console.log(power(2, 2));
-console.log(power(2, 3));
+console.log(power(2, 0)); // 1
+console.log(power(2, 1)); // 2
+console.log(power(2, 2)); // 4
+console.log(power(2, 3)); // 8
