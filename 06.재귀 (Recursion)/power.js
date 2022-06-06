2d43b8d15e36ba982010d2d0c3461233b3221b01
longlive-jonghan <kjhan710@naver.com>
2022-06-07 00:30:01 +0900

    재귀함수

---
+++ 06.재귀 (Recursion)/power.js
@@ -0,0 +1,17 @@
+// 제곱(Math.pow()) 구현하기
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+function power(a, b) {
+  if (b === 0) {
+    return 1;
+  }
+  b--;
+  return a * power(a, b);
+}
+
+console.log(power(2, 0));
+console.log(power(2, 1));
+console.log(power(2, 2));
+console.log(power(2, 3));
