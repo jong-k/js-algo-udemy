1018432d89d7fa0cec898886d6c1a7bacf684b1b
jong-k <kjhan710@naver.com>
2022-06-15 09:59:55 +0900

    refactor: hashTable

--- 22-해시-테이블/HashTable.js
+++ 22-해시-테이블/HashTable.js
@@ -1,18 +1,20 @@
 // hash table이 개별 체이닝 방식으로 동작하면, 인덱스가 나올 수 있는 범위를 우선 설정해야 함
 // 즉, 배열의 크기를 설정하는 것임
 // 그리고 키, 밸류를 그 인덱스에 저장하는 것임
-// hash 함수의 리턴값이 밸류가 아니고 인덱스임
+
 class HashTable {
-  constructor(size=53) { // 해시테이블의 크기를 설정 -> 디폴트 53으로 해봄
+  constructor(size = 53) { // 해시테이블의 크기를 설정 -> 디폴트 53으로 해봄
     this.keyMap = new Array(size); // 배열 생성
   }
 
+  // 키값을 입력받아 인덱스를 랜덤하게 리턴하는 함수
   _hash(key) {
     let total = 0;
-    let WEIRD_PRIME = 31;
+    const WEIRD_PRIME = 31;
     for (let i = 0; i < Math.min(key.length, 100); i++) {
-      let char = key[i];
-      let value = char.charCodeAt() - 96;
+      const char = key[i];
+      // a 의 아스키 코드 값이 97이므로 96을 빼줌
+      const value = char.charCodeAt() - 96; // a = 1, b = 2 ...
       total = (total * WEIRD_PRIME + value) % this.keyMap.length;
     }
     return total;
@@ -21,7 +23,7 @@ class HashTable {
   // 객체[키가 뭐든 문자열로 바뀜]
   // 객체.키를 문자열로 바꿔야 적용됨
   set(key, value) {
-    let index = this._hash(key);
+    const index = this._hash(key);
     if (!this.keyMap[index]) {
       this.keyMap[index] = [];
     }
@@ -29,7 +31,7 @@ class HashTable {
   }
 
   get(key) {
-    let index = this._hash(key);
+    const index = this._hash(key);
     if (!this.keyMap[index]) return undefined;
     for (let pair of this.keyMap[index]) {
       if (pair[0] === key) return pair[1];
