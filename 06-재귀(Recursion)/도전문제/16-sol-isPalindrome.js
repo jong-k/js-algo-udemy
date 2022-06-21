917b2f6f9a04112c9989bdb0150e821bb62dfbf0
jong-k <kjhan710@naver.com>
2022-06-21 22:58:54 +0900

    chore: 회문 문제 설명 주석 업데이트

--- 06-재귀(Recursion)/도전문제/16-sol-isPalindrome.js
+++ 06-재귀(Recursion)/도전문제/16-sol-isPalindrome.js
@@ -1,8 +1,8 @@
+// 문자열이 회문인지 아닌지를 출력하는 함수
+
 function isPalindrome(str) {
   if (str.length === 1) return true;
   if (str.length === 2) return str[0] === str[1];
   if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1));
   return false;
 }
-
-// 4번째 라인에서 str.slice(-1)로 마지막 글자 1개를 추출해냈음
