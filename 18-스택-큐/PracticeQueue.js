0fe115f82a3ec6dc86bfa0ef3ea0004996c13385
jong-k <kjhan710@naver.com>
2022-08-05 12:43:32 +0900

    220805 큐

---
+++ 18-스택-큐/PracticeQueue.js
@@ -0,0 +1,13 @@
+let q = [];
+
+q.push("FIRST")
+q.push("SECOND")
+q.push("THIRD")
+console.log(q); // FIRST, SECOND, THIRD
+
+// 스택에서는 pop을 썼지만, 큐에서는 shift를 사용한다
+// push-shift 조합 대신 unshift-pop 조합 사용도 가능
+q.shift(); // FIRST
+q.shift(); // SECOND
+q.shift(); // THIRD
+
---
+++ 18-스택-큐/Queue.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  // push
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+  // pop
+  dequeue() {
+    if (this.size === 0) return null;
+    const removedNode = this.first;
+    if (this.first === this.last) {
+      this.first = null;
+    } else {
+      this.first = this.first.next;
+    }
+    this.size--;
+    return removedNode;
+  }
+}
