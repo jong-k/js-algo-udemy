cab2c7be6c0bd00da0c295e7da3b3e6b4c88383e
jong-k <kjhan710@naver.com>
2022-08-05 17:50:06 +0900

    220805 이진 탐색 트리

---
+++ 19-트리/BinarySearchTree.js
@@ -0,0 +1,59 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    const newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (val === current.val) return undefined;
+      if (val > current.val) {
+        if (current.right) {
+            current = current.right;
+        } else {
+          current.right = newNode;
+          return this;
+        }
+      } else if (val < current.val) {
+        if (current.left) {
+          current = current.left;
+        } else {
+          current.left = newNode;
+          return this;
+        }
+      }
+    }
+  }
+
+  find(val) {
+    if (this.root === null) return undefined;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (val > current.val) {
+        current = current.right;
+      } else if (val < current.val) {
+        current = current.left;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
+}
+
+let tree = new BinarySearchTree();
