fda35c259ac66bae008ec6b77151c86021236b1b
jong-k <kjhan710@naver.com>
2022-08-25 19:54:22 +0900

    refactor: if 문 간소화

--- 07-탐색-알고리즘/sol_binary_search.js
+++ 07-탐색-알고리즘/sol_binary_search.js
@@ -4,14 +4,14 @@ function binarySearch1(arr, elem) {
     let end = arr.length - 1;
     let middle = Math.floor((start + end) / 2);
     while (arr[middle] !== elem && start <= end) {
-        if (elem < arr[middle]){
+        if (elem < arr[middle]) {
             end = middle - 1;
         } else {
             start = middle + 1;
         }
         middle = Math.floor((start + end) / 2);
     }
-    if (arr[middle] === elem){
+    if (arr[middle] === elem) {
         return middle;
     }
     return -1;
