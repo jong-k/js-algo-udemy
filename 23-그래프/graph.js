0e14f8242da90cc3cd022e840da56a3194e0fb7a
jong-k <kjhan710@naver.com>
2022-09-04 17:49:36 +0900

    220904 그래프 기초 복습

--- 23-그래프/graph.js
+++ 23-그래프/graph.js
@@ -14,12 +14,8 @@ class Graph {
   }
 
   removeEdge(v1, v2) {
-    this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => {
-      return v !== v2;
-    });
-    this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => {
-      return v !== v1;
-    });
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1);
   }
 
   // removeVertex(name) {
